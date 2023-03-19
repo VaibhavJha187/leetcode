84ca1dd99b82acdc2330f25f239218cce18ce48b
VaibhavJha187 <99673354+VaibhavJha187@users.noreply.github.com>
2023-03-19 12:09:29 +0530

    Time: 7 ms (6.83%), Space: 5.9 MB (62.26%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        for(int i= 0; i<=30; i++){
+            
+           long long int ans = pow(4,i);
+            
+            if(ans == n)
+                return 1;
+        }
+        
+        return 0;
+    }
+};
