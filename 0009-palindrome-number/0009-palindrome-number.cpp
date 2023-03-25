66d91a3e7887022ba447d2bcdec9733c9129ec2f
VaibhavJha187 <99673354+VaibhavJha187@users.noreply.github.com>
2023-03-25 14:15:00 +0530

    Time: 15 ms (66.23%), Space: 6 MB (15.24%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -2,10 +2,13 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         string s = to_string(x);
+        
         cout<<s;
-        string temp = s;
+        
+        string temp = s ;
         
         reverse(s.begin(), s.end());
+        
         return temp == s;
     }
 };
