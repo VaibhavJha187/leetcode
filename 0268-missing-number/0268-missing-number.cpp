659afcaa0c65782655d6a9db98e74c00379cb6a2
VaibhavJha187 <99673354+VaibhavJha187@users.noreply.github.com>
2023-03-25 19:57:33 +0530

    Time: 55 ms (6.29%), Space: 24.2 MB (5.42%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,77 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+       /* if(nums.size()==1 and nums[0]==1){
+            return 0;
+        }
+        else if (nums.size()==1 and nums[0]==0){
+            return 1;
+        }
+        
+        
+        sort(nums.begin(), nums.end());
+        
+        int s = 0;
+        int e = nums.size()-1;
+        
+        int mid = s +(e-s)/2;
+        
+        
+        while(s<=e){
+            
+        if(nums[mid+1] - nums[mid]== 1){
+               s= mid+1;
+            }
+    
+        else if(nums[mid+1] - nums[mid] ==2){
+            return nums[mid]+1;
+        }
+        
+        else if(nums[mid] - nums[mid-1] ==2){
+            return nums[mid]-1;
+        }
+            mid = s+(e-s)/2;
+            
+            if(s ==e){
+                return nums[mid]+1;
+            }
+            
+    }
+        
+    
+        
+        
+        s = 0; 
+        e = nums.size()-1;
+        
+         while(s<e){
+            
+           if(nums[mid]- nums[mid-1] == 1){
+            e = mid-1;
+         }
+    
+        else if(nums[mid+1] - nums[mid] ==2){
+            return nums[mid]+1;
+        }
+        
+        else if(nums[mid] - nums[mid-1] ==2){
+            return nums[mid]-1;
+        }
+          mid = s+(e-s)/2;
+    }
+        return -1;
+    }*/
+        unordered_map<int,int>map1;
+        for(auto i:nums){
+            map1[i]++;
+        }
+        
+        for(int i=0;i<=nums.size();i++){
+            if(map1.find(i)==map1.end()){
+                return i;
+            }
+        }
+        return -1;
+    }
+        
+};
